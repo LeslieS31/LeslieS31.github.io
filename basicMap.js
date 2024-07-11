d04654f667f2073efec52c91983b07971bf35409
LeslieS31 <leslie.staples2008@gmail.com>
2024-07-11 14:11:50 -0400

    Update basicMap.js

--- basicMap.js
+++ basicMap.js
@@ -3,7 +3,7 @@ console.log("JS File was loaded")
 //35.21665 N, 80.831473 W -> 35.21665, -80.831473
 
 let map= L.map("map",{
-  center:[35.21665, -80.831473],
+  center:[35.216665, -80.831473],
 zoom: 15
 });
 
