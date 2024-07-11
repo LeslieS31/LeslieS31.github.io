95e65366640d75a93c25e5f0a46d24fe5ec9d435
LeslieS31 <leslie.staples2008@gmail.com>
2024-07-11 14:02:10 -0400

    Update basicMap.js

--- basicMap.js
+++ basicMap.js
@@ -1,1 +1,12 @@
 console.log("JS File was loaded")
+
+//35.21665 N, 80.831473 W -> 35.21665, -80.831473
+
+let map= L.map("map",{
+  center:[35.21665, -80.831473],
+zoom: 8
+});
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
