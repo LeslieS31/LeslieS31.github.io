3062356562b66b3032ace9b31997d5efff6f6886
LeslieS31 <leslie.staples2008@gmail.com>
2024-07-16 14:46:57 -0400

    Update mexico.js

--- mexico.js
+++ mexico.js
@@ -12,7 +12,7 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 // chapultepec castle coordinates 19.4204, -99.1819
 L.circle([19.4204, -99.1819], {
 fillColor: '#326317', 
-  color: '#809e8c',
+  color: '#801b16',
   radius:600
 }).bindPopup(`<h2>Chapultepec Castle</h2><iframe width="280" height="160" src="https://www.youtube.com/embed/oFF4vxDIgEE?si=0g58ooqVeoGLFnP4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`)
   .addTo(map);
