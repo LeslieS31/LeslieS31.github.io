bc296615372575405c04d9722bf8592351b2169b
LeslieS31 <leslie.staples2008@gmail.com>
2024-07-16 14:43:26 -0400

    Update mexico.js

--- mexico.js
+++ mexico.js
@@ -14,4 +14,5 @@ L.circle([19.4204, -99.1819], {
 fillColor: '#326317', 
   color: '#809e8c',
   radius:600
-}).addTo(map);
+}).bindPopup(`<h2>Chapultepec Castle</h2><iframe width="280" height="160" src="https://www.youtube.com/embed/oFF4vxDIgEE?si=0g58ooqVeoGLFnP4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`)
+  .addTo(map);
